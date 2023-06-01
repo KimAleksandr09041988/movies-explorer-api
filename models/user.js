2a646061e13938019d3b2556edba51375b200e55
Kim Aleksandr <a.kim@jelata.tech>
2023-06-01 10:29:41 +0700

    fix отправка пароля в ответе при регистрации

--- models/user.js
+++ models/user.js
@@ -1,27 +1,30 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
-  },
-  password: {
-    type: String,
-    required: true,
-    select: false,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: {
-      validator: (value) => validator.isEmail(value),
-      message: 'Введён невалидный адрес электронной почты.',
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 30,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (value) => validator.isEmail(value),
+        message: 'Введён невалидный адрес электронной почты.',
+      },
     },
   },
-});
+  { toObject: { useProjection: true }, toJSON: { useProjection: true } },
+);
 
 module.exports = mongoose.model('user', userSchema);
