1a872a32c1254eadeb584c2bf8f0748e0d7f19b4
Kim Aleksandr <a.kim@jelata.tech>
2023-06-14 17:37:57 +0700

    fix

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,8 +1,8 @@
 const allowedCors = [
   'http://localhost:3000',
   'https://localhost:3000',
-  'http://movies.kim.nomoredomains.rocks/signup',
-  'https://movies.kim.nomoredomains.rocks/signup',
+  'http://movies.kim.nomoredomains.rocks',
+  'https://movies.kim.nomoredomains.rocks',
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
