e075bde600a1c33f603cbd9d6b0726e67b01601e
Kim Aleksandr <a.kim@jelata.tech>
2023-06-16 14:34:13 +0700

    fix

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -9,13 +9,13 @@ const allowedCors = [
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 module.exports = (req, res, next) => {
-  const { method } = req;
   const { origin } = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
   res.header("Access-Control-Allow-Credentials", true);
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
+  const { method } = req;
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
